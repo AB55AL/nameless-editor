cc90b492964dd75c9cdb2fc29753834bf838acbc
ab55al <abdullah5590x@gmail.com>
2022-07-03 21:17:28 +0300

    file_io: First file_io commit Added openFile() and writeToFile()

---
+++ src/file_io.zig
@@ -0,0 +1,69 @@
+const std = @import("std");
+const print = std.debug.print;
+const fs = std.fs;
+
+const Buffer = @import("buffer.zig");
+
+/// Opens a file and returns a pointer to a buffer
+pub fn openFile(allocator: std.mem.Allocator, file_path: []const u8) !*Buffer {
+    const full_file_path = try fullFilePath(allocator, file_path);
+    defer allocator.free(full_file_path);
+
+    const file = try fs.cwd().openFile(full_file_path, .{});
+    defer file.close();
+    const metadata = try file.metadata();
+    try file.seekTo(0);
+    var buf = try file.readToEndAlloc(allocator, metadata.size());
+    defer allocator.free(buf);
+
+    return Buffer.init(allocator, full_file_path, buf);
+}
+
+pub fn writeToFile(buffer: *Buffer) !void {
+    const new_file_suffix = ".editor-new";
+    const original_file_suffix = ".editor-original";
+
+    const new_file_path = try std.mem.concat(buffer.allocator, u8, &[_][]const u8{
+        buffer.file_path,
+        new_file_suffix,
+    });
+    defer buffer.allocator.free(new_file_path);
+
+    const original_tmp_file_path = try std.mem.concat(buffer.allocator, u8, &[_][]const u8{
+        buffer.file_path,
+        original_file_suffix,
+    });
+    defer buffer.allocator.free(original_tmp_file_path);
+
+    const content_of_buffer = try buffer.copyOfRows(1, @intCast(i32, buffer.lines.length()));
+    defer buffer.allocator.free(content_of_buffer);
+
+    const file_dir = &(try fs.openDirAbsolute(fs.path.dirname(buffer.file_path).?, .{}));
+    defer file_dir.close();
+
+    const new_file = try fs.createFileAbsolute(new_file_path, .{});
+    defer new_file.close();
+
+    try new_file.writeAll(content_of_buffer);
+    try std.os.rename(buffer.file_path, original_tmp_file_path);
+    try std.os.rename(new_file_path, buffer.file_path);
+    try file_dir.deleteFile(original_tmp_file_path);
+}
+
+pub fn fullFilePath(allocator: std.mem.Allocator, file_path: []const u8) ![]const u8 {
+    var full_file_path: []u8 = undefined;
+
+    if (fs.path.isAbsolute(file_path)) {
+        full_file_path = try allocator.alloc(u8, file_path.len);
+        std.mem.copy(u8, full_file_path, file_path);
+    } else {
+        var buf: [std.fs.MAX_PATH_BYTES]u8 = undefined;
+        var cwd_path = try std.os.getcwd(&buf);
+        full_file_path = try fs.path.join(allocator, &[_][]const u8{
+            cwd_path,
+            file_path,
+        });
+    }
+
+    return full_file_path;
+}
