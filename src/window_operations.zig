719bb16d5c76647d549ab65d64b8063a7163f9f4
ab55al <abdullah5590x@gmail.com>
2022-07-31 03:52:10 +0300

    core: add window_operations

---
+++ src/window_operations.zig
@@ -0,0 +1,18 @@
+const global_types = @import("global_types.zig");
+const Global = global_types.Global;
+const GlobalInternal = global_types.GlobalInternal;
+const command_line = @import("command_line.zig");
+
+extern var global: Global;
+extern var internal: GlobalInternal;
+
+pub fn cycleThroughWindows() void {
+    if (internal.windows.wins.items.len == 0) return;
+    if (global.command_line_is_open) command_line.close();
+    const static = struct {
+        var i: usize = 0;
+    };
+    static.i += 1;
+    if (static.i >= internal.windows.wins.items.len) static.i = 0;
+    global.focused_buffer = internal.windows.wins.items[static.i].buffer;
+}
