65e56f2d3ee4470ad7fd7d993882e1b08dd77404
ab55al <abdullah5590x@gmail.com>
2022-08-01 02:21:35 +0300

    build: ...

--- build.zig
+++ build.zig
@@ -19,17 +19,8 @@ pub fn buildEditor(bob: *Builder, comptime input_layer_path: []const u8) void {
     exe.setBuildMode(std.builtin.Mode.Debug);
     exe.linkLibC();
     exe.addIncludeDir(comptime thisDir() ++ "/src/ui/glad/include");
-    exe.addCSourceFile(comptime thisDir() ++ "/src/ui/glad/glad.c", &[_][]const u8{
-        "-lc",
-        "-lglfw3",
-        "-lGL",
-        "-lX11",
-        "-lpthread",
-        "-lXrandr",
-        "-lXi",
-        "-ldl",
-        "-I/usr/include",
-    });
+    exe.addCSourceFile(comptime thisDir() ++ "/src/ui/glad/glad.c", &[_][]const u8{});
+
     exe.addPackage(glfw.pkg);
     exe.addPackage(freetype.pkg);
     exe.addPackage(freetype.harfbuzz_pkg);
@@ -49,14 +40,14 @@ pub fn buildEditor(bob: *Builder, comptime input_layer_path: []const u8) void {
     const run_cmd = exe.run();
     run_cmd.step.dependOn(bob.getInstallStep());
 
-    const run_step = bob.step("run", "Run the app");
+    const run_step = bob.step("run", "Run the editor");
     run_step.dependOn(&run_cmd.step);
 
     const tests = bob.addTest(comptime thisDir() ++ "/tests/buffer.zig");
     tests.setBuildMode(std.builtin.Mode.Debug);
     tests.addPackagePath("core", comptime thisDir() ++ "/src/core.zig");
 
-    const test_step = bob.step("test", "Run library tests");
+    const test_step = bob.step("test", "Run tests");
     test_step.dependOn(&tests.step);
 }
 
