db6ba2eee84eaeba75202405dfe425002a214416
ab55al <abdullah5590x@gmail.com>
2022-10-31 10:38:26 +0300

    Standard-input-layer: nicer logging

--- src/input-layers/standard-input-layer/src/main.zig
+++ src/input-layers/standard-input-layer/src/main.zig
@@ -69,19 +69,21 @@ pub fn init() !void {
     try ft_mappings.addFileType(""); // Global and fallback file_type
     setDefaultMappnigs();
 
-    const data_path = std.os.getenv("XDG_DATA_HOME") orelse return;
-    const log_path = try std.mem.concat(allocator, u8, &.{ data_path, "/ne" });
-    defer allocator.free(log_path);
-    var dir = fs.openDirAbsolute(log_path, .{}) catch return;
-    defer dir.close();
+    {
+        const data_path = std.os.getenv("XDG_DATA_HOME") orelse return;
+        const log_path = std.mem.concat(allocator, u8, &.{ data_path, "/ne" }) catch return;
+        defer allocator.free(log_path);
+        var dir = fs.openDirAbsolute(log_path, .{}) catch return;
+        defer dir.close();
 
-    log_file = try (dir.openFile("input-log", .{ .mode = .write_only }) catch |err| if (err == fs.Dir.OpenError.FileNotFound)
-        dir.createFile("input-log", .{})
-    else
-        err);
+        log_file = (dir.openFile("input-log", .{ .mode = .write_only }) catch |err| if (err == fs.Dir.OpenError.FileNotFound)
+            dir.createFile("input-log", .{})
+        else
+            err) catch return;
 
-    const end = log_file.getEndPos() catch return;
-    _ = log_file.pwrite("\n---------------new editor instance---------------\n", end) catch |err| print("err={}", .{err});
+        const end = log_file.getEndPos() catch return;
+        _ = log_file.pwrite("\n---------------new editor instance---------------\n", end) catch |err| print("err={}", .{err});
+    }
 }
 
 pub fn deinit() void {
@@ -95,20 +97,11 @@ pub fn keyInput(key: Key) void {
     var k = ft_mappings.get(file_type, key);
     if (k) |f| {
         f();
-        const end = log_file.getEndPos() catch return;
-        var out: [20]u8 = undefined;
-        var key_str = key.toString(&out);
-
-        _ = log_file.pwrite(key_str, end) catch |err| print("err={}", .{err});
-        _ = log_file.pwrite("\n", end + key_str.len) catch |err| print("err={}", .{err});
+        logKey(key);
     } else if (file_type.len > 0) { // fallback
         if (ft_mappings.get("", key)) |f| {
             f();
-            const end = log_file.getEndPos() catch return;
-            var out: [20]u8 = undefined;
-            var key_str = key.toString(&out);
-            _ = log_file.pwrite(key_str, end) catch |err| print("err={}", .{err});
-            _ = log_file.pwrite("\n", end + key_str.len) catch |err| print("err={}", .{err});
+            logKey(key);
         }
     }
 }
@@ -152,6 +145,19 @@ fn setDefaultMappnigs() void {
     map(f(.none, .enter), enterKey);
     map(f(.none, .f1), toggleCommandLine);
 }
+
+fn logKey(key: Key) void {
+    const end = log_file.getEndPos() catch return;
+    var out: [20]u8 = undefined;
+    var key_str = key.toString(&out);
+
+    _ = log_file.pwrite(key_str, end) catch |err| print("err={}", .{err});
+    _ = log_file.pwrite("\n", end + key_str.len) catch |err| print("err={}", .{err});
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Function wrappers
+////////////////////////////////////////////////////////////////////////////////
 
 fn deleteBackward() void {
     global.focused_buffer.deleteBeforeCursor(1) catch |err| {
