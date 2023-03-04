c465c8c07d640a043bd7ae1d55908744226f40a8
ab55al <abdullah5590x@gmail.com>
2023-03-04 18:15:54 +0300

    core: remove editor.zig

--- src/core.zig
+++ src/core.zig
@@ -1,4 +1,15 @@
-pub usingnamespace @import("editor.zig");
+pub const Buffer = @import("editor/buffer.zig");
+pub const command_line = @import("editor/command_line.zig");
+pub const input = @import("editor/input.zig");
+pub const common_input_functions = @import("editor/common_input_functions.zig");
+pub usingnamespace @import("editor/buffer_ops.zig");
+pub usingnamespace @import("ui/notify.zig");
+pub usingnamespace @import("ui/buffer.zig");
+
+pub const globals = @import("globals.zig");
+pub const editor = globals.editor;
+pub const ui = globals.ui;
+
 pub const motions = @import("extras/motions.zig");
 pub const utils = @import("utils.zig");
 pub const utf8 = @import("utf8.zig");
